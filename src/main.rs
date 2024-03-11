6d46fdd4986051bfefd8eddc6653e40c4db11b16
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2024-03-11 19:11:54 +0100

    feat: use serde

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 use clap::{App, Arg};
 use git2::{Commit, ObjectType, Oid, Repository, Signature};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::fs::OpenOptions;
-use std::io::{BufRead, BufReader, Seek, SeekFrom, Write};
+use std::fs::{self};
 use std::path::Path;
 
-fn main() -> std::io::Result<()> {
-  let mut line = String::new();
-  let mut current_position = 0;
-  let mut old_version = String::new();
+mod version;
 
-  let mut splitting_char_map: HashMap<String, char> = HashMap::new();
-  //TODO Handle config for different extension in the map
-  // for example key, quotes, etc..
-  // {
-  //    key: "version"
-  //    splitting_char: '='
-  //
-  // }
-  splitting_char_map.insert(String::from("package.json"), ':');
-  splitting_char_map.insert(String::from("cargo.toml"), '=');
+#[derive(Debug, Serialize, Deserialize)]
+struct Package {
+  version: version::Version,
+  #[serde(flatten)]
+  other: HashMap<String, serde_json::Value>,
+}
+
+fn main() -> serde_json::Result<()> {
+  let data = fs::read_to_string("package.json").expect("Unable to read file");
+
+  let mut package_json: Package =
+    serde_json::from_str(&data).expect("Package json should contain a version field.");
 
   let matches = App::new("pvb")
     .arg(
@@ -51,70 +50,38 @@ fn main() -> std::io::Result<()> {
     // )
     .get_matches();
 
-  let path = matches.value_of("file").unwrap_or("package.json");
-
-  let mut file = OpenOptions::new().read(true).write(true).open(path)?;
-
-  let mut reader = BufReader::new(&file);
-
-  while reader.read_line(&mut line)? > 0 {
-    if line.trim_start().to_lowercase().contains("version") {
-      let splitting_char = match splitting_char_map.get(path) {
-        Some(character) => *character,
-        None => ':',
-      };
-
-      let parts: Vec<&str> = line.split(splitting_char).collect();
-      println!(" splitting {} line {}", splitting_char, line);
-
-      if parts.len() >= 2 {
-        old_version = parts[1]
-          .trim()
-          .chars()
-          .filter(|c| !(*c == '"' || *c == '\n' || *c == ','))
-          .collect::<String>();
-        break;
-      } else {
-        println!("Invalid format for version line in package.json");
-        return Ok(());
-      }
-    }
-    current_position += line.len() + 1;
-    line.clear();
-  }
-
-  let version = match (
+  match (
     matches.is_present("major"),
     matches.is_present("minor"),
     matches.is_present("patch"),
   ) {
-    (true, _, _) => increment_major_version(&old_version),
-    (_, true, _) => increment_minor_version(&old_version),
-    (_, _, true) => increment_patch_version(&old_version),
+    (true, _, _) => package_json.version.increment_major(),
+    (_, true, _) => package_json.version.increment_minor(),
+    (_, _, true) => package_json.version.increment_patch(),
     _ => {
       println!("No version increment option specified.");
       return Ok(());
     }
   };
-  println!("Current version is: {}", old_version);
-  println!("Update the version to: {}? [y/N]", version);
-  let test = String::from("test");
 
-  println!("{}", test);
+  let version = package_json.version.to_string();
+
+  println!("Update the version to: {}? [y/N]", version);
+
   let mut confirmation = String::new();
-  std::io::stdin().read_line(&mut confirmation)?;
+  std::io::stdin()
+    .read_line(&mut confirmation)
+    .expect("Cannot read input.");
 
   if confirmation.trim().to_lowercase() == "y" {
-    // Seek to the version line position and overwrite the entire line
-    file.seek(SeekFrom::Start(current_position as u64))?;
-    let updated_line = format!("\"version\": \"{}\"", version);
-    file.write_all(updated_line.as_bytes())?;
-    println!("Version updated successfully!");
+    let value = serde_json::to_string_pretty(&package_json)?;
+
+    fs::write("package.json", value).expect("Unable to write file");
 
     let repo_root = ".";
     let repo = Repository::open(repo_root).expect("Couldn't open repository");
 
-    let relative_path = Path::new(&path);
+    let relative_path = Path::new("package.json");
 
     add_and_commit(&repo, relative_path, &version).expect("Couldn't add file to repo");
   } else {
@@ -122,27 +89,6 @@ fn main() -> std::io::Result<()> {
   }
 
   Ok(())
-}
-
-fn increment_major_version(current_version: &str) -> String {
-  let parts: Vec<&str> = current_version.split('.').collect();
-  let major: u64 = parts[0].parse().unwrap_or(0);
-  format!("{}.{}.{}", major + 1, 0, 0)
-}
-
-fn increment_minor_version(current_version: &str) -> String {
-  let parts: Vec<&str> = current_version.split('.').collect();
-  let major: u64 = parts[0].parse().unwrap_or(0);
-  let minor: u64 = parts[1].parse().unwrap_or(0);
-  format!("{}.{}.{}", major, minor + 1, 0)
-}
-
-fn increment_patch_version(current_version: &str) -> String {
-  let parts: Vec<&str> = current_version.split('.').collect();
-  let major: u64 = parts[0].parse().unwrap_or(0);
-  let minor: u64 = parts[1].parse().unwrap_or(0);
-  let patch: u64 = parts[2].parse().unwrap_or(0);
-  format!("{}.{}.{}", major, minor, patch + 1)
 }
 
 fn add_and_commit(repo: &Repository, path: &Path, version: &str) -> Result<Oid, git2::Error> {
